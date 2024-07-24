1cf16babb9f327db10ddeb17716f1e345ea02ed2
Samuel <samuelint@gmail.com>
2024-07-24 16:56:45 -0400

    fix: export input & output at root

--- base_assistant_extension/__init__.py
+++ base_assistant_extension/__init__.py
@@ -1,3 +1,3 @@
-from .base_extension import BaseExtension
+from .base_extension import BaseExtension, ExtensionInput, ExtensionOutput
 
-__all__ = ["BaseExtension"]
+__all__ = ["BaseExtension", "ExtensionInput", "ExtensionOutput"]
