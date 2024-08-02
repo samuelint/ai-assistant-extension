9d32eef856fbeb57a13b1741163a471aa5f21a08
Samuel <samuelint@gmail.com>
2024-08-02 11:12:48 -0400

    feat: improve __init__

--- base_assistant_extension/__init__.py
+++ base_assistant_extension/__init__.py
@@ -1,3 +1,5 @@
 from .base_extension import BaseExtension, ExtensionInput
+from .extension_host import ExtensionHost
+from .extension_metadata import ExtensionMetadata
 
-__all__ = ["BaseExtension", "ExtensionInput"]
+__all__ = ["BaseExtension", "ExtensionInput", "ExtensionHost", "ExtensionMetadata"]
